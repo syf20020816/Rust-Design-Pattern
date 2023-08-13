9bb49d53e7d96ad647f1a0fd100f4b9f6a53cf0a
syf20020816 <syf20020816@outlook.com>
2023-08-13 15:55:34 +0800

    20230813-5-design_pattern:strategy finish

--- design_pattern/strategy/src/main.rs
+++ design_pattern/strategy/src/main.rs
@@ -1,3 +1,10 @@
+mod lib;
+
+use crate::lib::{Context, Strategy, ShowStrategy1, ShowStrategy2};
+
 fn main() {
-    println!("Hello, world!");
+    let mut context: Context<dyn Strategy> = Context::new(Box::new(ShowStrategy1));
+    context.show();
+    context.replace(Box::new(ShowStrategy2));
+    context.show();
 }
